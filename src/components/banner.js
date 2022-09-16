3d19028d8b16293cf1c321cc132714ed37b57ea7
Vimal Kumar <kvimal9560@gmail.com>
2022-09-17 00:41:02 +0530

    initial commit

---
+++ src/components/banner.js
@@ -0,0 +1,15 @@
+export const Banner=(props)=>{
+return (
+    <>
+
+  <div className="banner row">
+    <div className="col-md-12" style={{padding:'0'}}>
+    {/* <img className="d-block w-100" src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2022/BAU/ATFGW/Best_find_postpe_coupon_3000x1200._CB635819533_.jpg" alt="First slide"/> */}
+    
+      <img className="d-block w-100" src="https://images.pexels.com/photos/271816/pexels-photo-271816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="First slide"/>
+    </div>
+  </div>
+    </>
+)
+
+}
